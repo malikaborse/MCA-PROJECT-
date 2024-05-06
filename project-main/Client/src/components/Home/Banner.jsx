48e2ae69fbe8d3833c8cb896e55a943227b17086
malikaborse <kamolica30@gmail.com>
2024-05-06 13:36:05 +0530

    banneer

--- project-main/Client/src/components/Home/Banner.jsx
+++ project-main/Client/src/components/Home/Banner.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import backgroundImage from '../../assets/Food.jpg';
+import backgroundImage from '../../assets/bg_1.jpg';
 
 const Banner = () => {
     return (
         <section className="banner relative h-screen overflow-hidden bg-cover bg-center flex justify-center items-center">
-            <img src={backgroundImage} className="absolute z-0 inset-0 mx-auto my-auto w-[90vw] h-[80vh] object-cover rounded-xl" />
-            <div className="banner-content text-center text-white flex flex-col justify-center items-center sm:w-[80vw] lg:w-[65vw] backdrop-filter backdrop-blur-sm bg-white bg-opacity-20 rounded-lg shadow-md px-6 w-[80vw] h-[70vh] sm:h-[50vh] animate-fade-in">
-                <h1 className="text-6xl font-medium tracking-wider mb-8 text-black">Welcome To <span className='px-2 py-1 bg-gradient-to-r from-orange-500 from-30% via-sky-500 via-50% to-emerald-500 to-90% rounded-lg inline-block text-transparent bg-clip-text' style={{ fontVariant: 'unicase' }}>Food Space</span></h1>
-                <h3 className="text-xl font-semibold mb-12 border-b border-gray-400 pb-2 ml-4 mr-4 text-white">
-                    We deliver not to "Escape Hunger", but for Flavor not to "Escape Us".
+            <img src={backgroundImage} className="absolute z-0 inset-0 mx-auto my-auto w-[100vw] h-[100vh] " />
+            <div className="banner-content text-center text-white flex flex-col justify-center items-center sm:w-[80vw] lg:w-[65vw] backdrop-filter backdrop-blur-sm bg-white bg-opacity-10 rounded-lg shadow-md px-2 w-[150vw] h-[50vh] sm:h-[40vh] animate-fade-in">
+                <h1 className="text-5xl font-medium tracking-wider mb-10 text-black">Welcome To <span className='px-2 py-1 bg-gradient-to-r from-orange-500 from-30% via-sky-500 via-50% to-emerald-500 to-90% rounded-lg inline-block text-transparent bg-clip-text' style={{ fontVariant: 'unicase' }}> Greenskape studio
+</span></h1>
+                <h3 className="text-xl font-semibold mb-8 border-b border-gray-400 pb-2 ml-4 mr-4 text-gray-700">
+                We Create Amazing Interior/Architecture Designs
                 </h3>
                 <Link to="#">
                     <button
@@ -21,7 +22,7 @@ const Banner = () => {
                             whiteSpace: 'nowrap'
                         }}
                     >
-                        - Explore More -
+                        view our works
                     </button>
                 </Link>
             </div>
